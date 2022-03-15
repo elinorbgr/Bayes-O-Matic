13fbdc39d44bc9cb0afeba2c0ca7be649f6fc126
vberger <vberger@users.noreply.github.com>
2022-03-15 22:46:07 +0000

    deploy: 7599947b41d7a92d4d680f4e8c100e87ba4aafcc

--- index-ea9acfec2409e38d.js
+++ index-ea9acfec2409e38d.js
@@ -299,7 +299,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-e80b5b03d1ccbf79_bg.wasm', import.meta.url);
+        input = new URL('index-ea9acfec2409e38d_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
